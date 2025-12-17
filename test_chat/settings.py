a552c05b0b776db8f96d8e282ea2ba239633aa64
Frol69 <Frolov27101993@yandex.ru>
2025-12-17 14:50:13 +0300

    удаление лишней директории, создание модели сообщения

--- test_chat/settings.py
+++ test_chat/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'chat',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'test_chat_.urls'
+ROOT_URLCONF = 'test_chat.urls'
 
 TEMPLATES = [
     {
@@ -66,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'test_chat_.wsgi.application'
+WSGI_APPLICATION = 'test_chat.wsgi.application'
 
 
 # Database
